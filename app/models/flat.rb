1817af005b9580d974d4bf22fdce67a87c48cc69
Piotr Damer <piotrd@czuby.net>
2020-02-19 15:29:24 +0000

    Built the basic front-end and back-end functionality

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,5 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+  validates :price_per_night, numericality: { only_integer: true, greater_than: 49, less_than: 501 }
+  validates :number_of_guests, numericality: { only_integer: true, greater_than: 0, less_than: 11 }
 end
