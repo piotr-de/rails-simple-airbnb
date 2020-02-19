ec7e5b97197ddf46c47a4ea22be18343fb98c221
Piotr Damer <piotrd@czuby.net>
2020-02-19 14:01:18 +0000

    Generated the modela and controller for the AirBnB lite app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+  Flat.create!(
+  name: Faker::Marketing.buzzwords,
+  address: Faker::Address.full_address,
+  description: Faker::ChuckNorris.fact,
+  price_per_night: rand(50..500),
+  number_of_guests: rand(1..10)
+)
+end
